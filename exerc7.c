b289f608e0db76df959ee6628c71afc95b34c571
BRUNO OLIVEIRA <brunnomoraaes@gmail.com>
2022-09-23 14:20:49 -0300

    questao 7 resolvida

--- exerc7.c
+++ exerc7.c
@@ -1,4 +1,4 @@
-// Implemente um programa que solicita as notas das duas provas feitas por cada um dos alunos de uma turma (as notas tÃªm de estar no intervalo [0 10]) e imprime para cada um a mÃ©dia das notas. O programa deve parar imediatamente apÃ³s ter sido digitado o valor 50 para a nota da primeira prova.
+// Implemente um programa que solicita as notas das duas provas feitas por cada um dos alunos de uma turma (as notas têm de estar no intervalo [0 10]) e imprime para cada um a média das notas. O programa deve parar imediatamente após ter sido digitado o valor 50 para a nota da primeira prova.
 
 #include <stdio.h>
 #include <locale.h>
@@ -6,5 +6,32 @@
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-    int i;
+    float nota1, nota2;
+    char continuar;
+
+    
+    do 
+    {
+        // Armazenando as notas do aluno
+        printf("Digite a primeira nota do aluno:");
+        scanf("%f", &nota1);
+        
+        if (nota1 != 50)
+        {
+            printf("Digite a segunda nota do aluno:");
+            scanf("%f", &nota2);
+
+            // Cálculo da média
+
+            float media = (nota1 + nota2) / 2;
+
+            printf("%.2f\n\n", media);
+        }
+
+        // Verifica se o usuário deseja calcular a média de outro aluno.
+        printf("Deseja calcular a média de outro aluno?");
+        scanf("%s", &continuar);
+        continuar = toupper(continuar);
+
+    } while ((continuar == 'S'));
 }
